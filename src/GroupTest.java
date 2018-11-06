5789223419d6e13669530b09fa888776b225fca8
AlexDresco <alexandra.d1510@gmail.com>
2018-11-06 11:54:44 +0000

    Groups

---
+++ src/GroupTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupTest {
+
+    Group group;
+
+    @BeforeEach
+    void init(){
+        group = new Group();
+    }
+
+
+    @Test
+    void groupExists(){
+        assertArrayEquals(new String[]{"Patrick", "Sebastian", "Nazz", "Amy", "Chloe", "Din", "Damion", "Ruaridhi", "Freya", "Victor", "Shweta", "Nathan", "Alex", "Vu", "Robbi", "Ibs"}, group.aug.toArray());
+    }
+
+    @Test
+    void numberofGroupsExist(){
+        assertEquals(3, group.numberOfGroups);
+    }
+
+
+    @Test
+    void sizeOfGroups(){
+        assertEquals(5, group.membersPerGroup);
+    }
+
+
+    @Test
+    void divide(){ group.divide(); }
+
+}
--- src/Timing.java
+++ src/Timing.java
@@ -1,7 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 
 public class Timing {
 
@@ -9,26 +6,37 @@ public class Timing {
         public static void main(String[] args){
             int[] range = {10,100,1000,3000,5000,10000,30000,50000,100000,300000,500000,1000000};
             for (int arraySize : range) {
-                long startTime = System.nanoTime();
-                testableMethod(arraySize);
-                long endTime = System.nanoTime();
-                long duration = (endTime - startTime);
-                System.out.println(duration);
+long startTime = System.nanoTime();
+testableMethod(arraySize);
+long endTime = System.nanoTime();
+long duration = (endTime - startTime);
+System.out.println(duration);
             }
 
         }
 
 
         public static void testableMethod(int arraySize) {
-            ArrayList<Integer> array = new ArrayList<Integer>();
+            //Set up a list
+            ArrayList<Integer> aug = new ArrayList<Integer>();
             for (int i = 0; i < arraySize; i++){
-                array.add(i);
+                aug.add(i);
             }
-            Collections.shuffle(array);
-            Collections.sort(array);
+
+            // Set how many groups
+            int numberOfGroups = 3;
+
+            // Divide the number of people by groups
+            int membersPerGroup = aug.size() / numberOfGroups;
+
+            // Create equal groups
+            for (int start = 0; start < aug.size(); start += membersPerGroup) {
+                int end = Math.min(start + membersPerGroup, aug.size());
+                List<Integer> group = aug.subList(start, end);
+            // Show the groups
+//            System.out.println(group);
+            }
         }
-
-
     }
 
 
